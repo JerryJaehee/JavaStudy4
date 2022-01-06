8f47a1e5fe3e1c4859c8fa17c0ba190e5c4cc993
jerry <woffl_@naver.com>
2022-01-06 22:00:17 +0900

    commit

--- src/com/iu/s4/SquareMain.java
+++ src/com/iu/s4/SquareMain.java
@@ -4,7 +4,7 @@ public class SquareMain {
 
 	public static void main(String[] args) {
 		Square square = new Square();
-		
+		System.out.println();
 	//	square.sero = 10;
 		square.info();
 		square.info2();
